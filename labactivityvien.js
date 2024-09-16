22920631845a79e4e8e8fd3f21ca73b5fd3708d8
Vien Galang <viengalang@Viens-MacBook-Air.local>
2024-09-16 10:43:30 -0700

    lab act

---
+++ labactivityvien.js
@@ -0,0 +1,15 @@
+
+const day = "Saturdays"; //best day ever - Bruna
+
+let hobby = "watch tv show";
+
+console.log(`During my past time, my favorite thing to do is ${hobby}. I usually do it on ${day}.`);
+
+hobby = "crocheting"; //Love that you how to do this, I want to learn too - Bruna
+
+console.log(`But during the pandemic, I was able to learn a new skill which is ${hobby}. I did learn it along with my friend. I was struggling at first but when I got the hang of it, it was really fun. At first, I only bought ` + 2 + ` balls of cotton yarn, eventually purchased a lot more. So now, during ${day}, you will find me ${hobby}.`); // This way of writing the code it's really cool, avoiding so much trouble with quotes and pluses - Bruna
+
+
+
+
+
